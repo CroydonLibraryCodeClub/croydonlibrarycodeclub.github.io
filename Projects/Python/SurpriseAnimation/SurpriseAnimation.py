89fbded0e9ec337f882c6d90eaa545df33cd178e
Paul Sinnett <paul.sinnett@gmail.com>
2021-12-13 12:14:47 +0000

    rewrite with clock

--- Projects/Python/SurpriseAnimation/SurpriseAnimation.py
+++ Projects/Python/SurpriseAnimation/SurpriseAnimation.py
@@ -5,58 +5,38 @@ TITLE = 'BOO!'
 WIDTH = 480
 HEIGHT = 360
 
-hedgehog = Actor('hedgehog-a', (200, 180))
+hedgehog = Actor('hedgehog-a', (150, 272))
 hedgehog.text = None
-apple = Actor('apple', (280, 180))
+apple = Actor('apple', (351, 272))
 apple.text = None
+hedgehog.walk = ['hedgehog-a', 'hedgehog-b']
 
-def wait(seconds):
-	end = time.time() + seconds
-	while time.time() < end:
-		yield
-
-def hedgehog_script():
-	hedgehog.pos = (150, 272)
-	yield from wait(1.0)
+def think():
 	hedgehog.text = 'Hmm..'
-	for _ in range(20):
-		hedgehog.image = 'hedgehog-a'
-		hedgehog.x += 3
-		yield from wait(0.1)
-		hedgehog.image = 'hedgehog-b'
-		hedgehog.x += 3
-		yield from wait(0.1)
-	hedgehog.text = None
-	yield from wait(1.0)
-	hedgehog.image = 'hedgehog-d'
+	step()
 
-def apple_script():
-	apple.pos = (351, 272)
-	yield from wait(6.0)
+def step():
+	hedgehog.image = hedgehog.walk[0]
+	hedgehog.x += 3
+	if hedgehog.colliderect(apple):
+		surprise()
+	else:
+		hedgehog.walk.reverse()
+		clock.schedule(step, 0.1)
+
+def surprise():
 	apple.image = 'ghost'
 	apple.text = 'BOO!'
+	hedgehog.image = 'hedgehog-d'
+	hedgehog.text = None
 	sounds.laugh.play()
-
-def update():
-	done = []
-	for script in scripts:
-		try:
-			next(script)
-		except StopIteration:
-			done.append(script)
-	for script in done:
-		scripts.remove(script)
 
 def draw():
 	screen.blit('background', (0, 0))
-	for sprite in sprites:
-		sprite.draw()
-		if sprite.text is not None:
-			screen.draw.text(
-				sprite.text,
-				midbottom = sprite.midtop
-			)
+	hedgehog.draw()
+	if hedgehog.text is not None:
+		screen.draw.text(hedgehog.text, midbottom = hedgehog.midtop)
+	apple.draw()
 
-sprites = [hedgehog, apple]
-scripts = [hedgehog_script(), apple_script()]
+clock.schedule(think, 1.0)
 pgzrun.go()
